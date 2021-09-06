d35f9ba9868dcc09503f373d670b354f55a4f584
Michael Angelo Cuento <michaelangelocuento@Michaels-MBP.fios-router.home>
2021-09-06 16:43:50 -0400

    <they are fighting now>

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,8 +10,8 @@ var enemyAttack = 12
 
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+  // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) {
   
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -61,9 +61,12 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 } else {
   window.alert("You need to choose a valid option. Try again!");
 }
+  }
     };
 
-for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
-}
+for (var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
+  }
 
